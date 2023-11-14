82d6d6ce161f01c82828b799e1bfe2f2ddc675a3
SeyaSix <gomat63@yahoo.fr>
2023-11-14 17:06:30 +0100

    css header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,33 +4,33 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="h-container">
-      <img src="/img/pikachu.jpg" alt="Pikachu" className="pikachu-image" />
+      <img src="/img/pikachu.jpg" alt="Pikachu" className="pikachu-image" width="70px"/>
       <h1>Pokedex</h1>
       <nav className="nav-container">
         <div className="nav-header">
           <p>Navigation</p>
           <ul className="nav-list">
             <li>
-              <Link to="/">Home</Link>
+            <button className="buttonNav"><Link to="/">Home</Link></button>
             </li>
             <li>
-              <Link to="/detail">details</Link>
-            </li>
-            <li>
-              {" "}
-              <Link to="/liste">liste</Link>
+            <button className="buttonNav"><Link to="/detail">Details</Link></button>
             </li>
             <li>
               {" "}
-              <Link to="/typeFire">Type Feu</Link>
+              <button className="buttonNav"> <Link to="/liste">Liste</Link></button>
             </li>
             <li>
               {" "}
-              <Link to="/typeWater">Type Eau</Link>
+              <button className="buttonNav"><Link to="/typeFire">Type Feu</Link></button>
             </li>
             <li>
               {" "}
-              <Link to="/typePoison">Type Poison</Link>{" "}
+              <button className="buttonNav"><Link to="/typeWater">Type Eau</Link></button>
+            </li>
+            <li>
+              {" "}
+              <button className="buttonNav"><Link to="/typePoison">Type Poison</Link></button>{" "}
             </li>
           </ul>
         </div>
